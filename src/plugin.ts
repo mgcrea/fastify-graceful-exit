d13c04f719d768e956df8b645a48e0f2e76ba040
Olivier Louvignes <olivier@mgcrea.io>
2021-03-29 10:44:05 +0200

    fix(events): add missing events (fixes #1)

--- src/plugin.ts
+++ src/plugin.ts
@@ -8,20 +8,27 @@ export type FastifyGracefulExitOptions = {
 export const plugin: FastifyPluginAsync<FastifyGracefulExitOptions> = async (fastify, options = {}): Promise<void> => {
   const { logBindings = { plugin: 'fastify-graceful-exit' }, timeout = 3000 } = options;
   const { log } = fastify;
+  let closePromise: ReturnType<typeof fastify.close> | null = null;
   // Gracefully close
   const gracefullyClose = async (signal: string) => {
+    if (closePromise) {
+      return closePromise;
+    }
     log.warn(logBindings, `Fastify is gracefully closing from signal="${signal}" ...`);
     // Exit after a 3s timeout
     setTimeout(() => {
       log.warn(logBindings, `Failed to gracefully close before timeout`);
       process.exit(1);
     }, timeout);
-    await fastify.close();
+    closePromise = fastify.close();
+    await closePromise;
     process.exit(0);
   };
-  process.once('SIGTERM', gracefullyClose);
+  process.on('uncaughtException', gracefullyClose);
+  process.on('unhandledRejection', gracefullyClose);
+  process.on('SIGTERM', gracefullyClose);
   // Handle Ctrl+C
-  process.once('SIGINT', gracefullyClose);
+  process.on('SIGINT', gracefullyClose);
   // Handle nodemon-like restarts
-  process.once('SIGUSR2', gracefullyClose);
+  process.on('SIGUSR2', gracefullyClose);
 };
